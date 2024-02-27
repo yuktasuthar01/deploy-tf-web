635013ac65f48e0daf48e19a2fd02895fb0c8b86
Yukta Suthar <82398952+yuktasuthar01@users.noreply.github.com>
2024-02-27 18:02:02 +0530

    Delete terraform directory

--- terraform/modules/service-plan/output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The ID of the App Service Plan"
-  value       = azurerm_service_plan.asp.id
-}
-
-output "name" {
-  description = "The Name of the App Service Plan"
-  value       = azurerm_service_plan.asp.name
-}
